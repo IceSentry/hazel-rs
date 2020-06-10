9ba5aabdcf81cf4146d241d14dad8c5a713bcbfd
IceSentry <c.giguere42@gmail.com>
2020-06-10 01:25:57 -0400

    more imgui cleanup

--- hazel-rs-lib/src/layers/imgui.rs
+++ hazel-rs-lib/src/layers/imgui.rs
@@ -5,12 +5,6 @@ use imgui::{im_str, Condition, FontSource};
 use imgui_winit_support::{HiDpiMode, WinitPlatform};
 use log::trace;
 
-struct ImguiState {
-    pub context: Box<imgui::Context>,
-    pub platform: Box<WinitPlatform>,
-    pub renderer: Box<imgui_wgpu::Renderer>,
-}
-
 #[derive(new)]
 pub struct ImguiLayer {
     #[new(default)]
@@ -24,10 +18,12 @@ impl Layer for ImguiLayer {
     }
 
     fn on_event(&mut self, app: &mut Application, event: &Event<()>) {
-        let state = self.state.as_mut().unwrap();
-        state
-            .platform
-            .handle_event(state.context.io_mut(), &app.window, &event);
+        if let Some(ImguiState {
+            platform, context, ..
+        }) = self.state.as_mut()
+        {
+            platform.handle_event(context.io_mut(), &app.window, &event);
+        }
     }
 
     fn on_render(
@@ -37,37 +33,46 @@ impl Layer for ImguiLayer {
         frame: &wgpu::SwapChainOutput,
     ) {
         let delta_t = app.delta_t;
-        let state = self.state.as_mut().unwrap();
-        state
-            .platform
-            .prepare_frame(state.context.io_mut(), &app.window)
-            .expect("Failed to prepare frame");
-
-        state.context.io_mut().delta_time = delta_t.as_secs_f32();
-        let ui = state.context.frame();
-
+        if let Some(ImguiState {
+            platform,
+            context,
+            renderer,
+        }) = self.state.as_mut()
         {
-            imgui::Window::new(im_str!("Debug info"))
-                .position([0.0, 0.0], Condition::FirstUseEver)
-                .build(&ui, || {
-                    ui.text(im_str!("Frametime: {:?}", delta_t));
-                    ui.separator();
-                    let mouse_pos = ui.io().mouse_pos;
-                    ui.text(im_str!(
-                        "Mouse Position: ({:.1},{:.1})",
-                        mouse_pos[0],
-                        mouse_pos[1]
-                    ));
-                });
+            platform
+                .prepare_frame(context.io_mut(), &app.window)
+                .expect("Failed to prepare frame");
+
+            context.io_mut().delta_time = delta_t.as_secs_f32();
+            let ui = context.frame();
+
+            {
+                imgui::Window::new(im_str!("Debug info"))
+                    .position([0.0, 0.0], Condition::FirstUseEver)
+                    .build(&ui, || {
+                        ui.text(im_str!("Frametime: {:?}", delta_t));
+                        ui.separator();
+                        let mouse_pos = ui.io().mouse_pos;
+                        ui.text(im_str!(
+                            "Mouse Position: ({:.1},{:.1})",
+                            mouse_pos[0],
+                            mouse_pos[1]
+                        ));
+                    });
+            }
+
+            platform.prepare_render(&ui, &app.window);
+            renderer
+                .render(ui.render(), &app.renderer.device, encoder, &frame.view)
+                .expect("imgui rendering failed");
         }
-
-        state.platform.prepare_render(&ui, &app.window);
-
-        state
-            .renderer
-            .render(ui.render(), &app.renderer.device, encoder, &frame.view)
-            .expect("imgui rendering failed");
     }
+}
+
+struct ImguiState {
+    context: Box<imgui::Context>,
+    platform: Box<WinitPlatform>,
+    renderer: Box<imgui_wgpu::Renderer>,
 }
 
 impl ImguiState {
